5d930a8afcc6f598e9c550cd5077c7a2559d942c
MinSeo <danalstj@gmail.com>
2021-03-28 16:49:46 +0900

    답범위놓치는버그해결,답버튼크기감소

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   Text,
   View,
   TouchableHighlight,
+  // TouchableOpacity,
   StatusBar,
   Alert,
 } from "react-native";
@@ -35,7 +36,7 @@ export default class extends React.Component {
     Qnum: 0,
     left: 1,
     mid: 500,
-    right: 1001,
+    right: 1000,
   };
 
   loadSound_PlayBGM = async () => {
@@ -50,6 +51,9 @@ export default class extends React.Component {
   playSound = async (sound) => {
     await sound.playAsync();
   };
+  replaySound = async (sound) => {
+    sound.replayAsync();
+  };
 
   stopSound = async (sound) => {
     await sound.stopAsync();
@@ -58,7 +62,7 @@ export default class extends React.Component {
   gameReset() {
     backUp = [];
     this.stopSound(soundFigureOut);
-    this.playSound(soundBGM);
+    this.replaySound(soundBGM);
     figureOut = false;
     this.setState({
       GameState: "Start",
@@ -71,15 +75,12 @@ export default class extends React.Component {
 
   constructor() {
     super();
-    Alert.alert("도움말", "자기보다 작냐고 물어보면 작은게 아니다!");
     this.loadSound_PlayBGM();
   }
 
   ask() {
-    if (
-      this.state.mid == this.state.right - 1 ||
-      this.state.mid - 1 == this.state.left
-    ) {
+    //// this.state.mid == this.state.right - 1 || && this.state.mid == this.state.left + 1
+    if (this.state.right - this.state.left <= 2) {
       this.state.askIsThat = true;
     } else {
       this.state.askIsThat = false;
@@ -111,15 +112,18 @@ export default class extends React.Component {
         this.state.mid =
           this.state.right -
           parseInt((this.state.right - this.state.left + 1) / 2);
-        this.state.Qnum == 10 ? (figureOut = true) : {}; //// 질문 10번이 끝나면 무조건 답을 찾아내게 되있는데,
       } else {
-        // 이것이냐고 콕 집어 물어보는 질문에 아니라고 답하면 답을 구한 줄 모름
         this.state.left = this.state.mid;
         this.state.mid =
           this.state.left +
           parseInt((this.state.right - this.state.left + 1) / 2);
       }
     }
+    //// 질문 10번이 끝나면 무조건 답을 찾아내게 되있는데, 이것이냐고 콕 집어 물어보는 질문에 아니라고 답하면 답을 구한 줄 모름
+    //// 남은 숫자가 하나인데도 답을 못 찾는 경우가 있음
+    this.state.right - this.state.left == 1 || this.state.Qnum == 10
+      ? (figureOut = true)
+      : {}; ////
     answerYes = false;
     answerNo = false;
     thinkEnd = true;
@@ -128,7 +132,7 @@ export default class extends React.Component {
   back() {
     if (figureOut) {
       this.stopSound(soundFigureOut);
-      this.playSound(soundBGM);
+      this.replaySound(soundBGM);
       figureOut = false;
     }
     if (this.state.Qnum == 0) {
@@ -156,17 +160,17 @@ export default class extends React.Component {
 
     // 시작 또는 끝내기, 뒤로가기 버튼 눌렀을 때
     if (answerStart) {
-      this.playSound(soundAnswer);
+      this.replaySound(soundAnswer);
       this.ask();
       answerStart = false;
       return;
     } else if (answerEnd) {
-      this.playSound(soundEnd);
+      this.replaySound(soundEnd);
       this.gameReset();
       answerEnd = false;
       return;
     } else if (answerBack) {
-      this.playSound(soundBack);
+      this.replaySound(soundBack);
       this.back();
       answerBack = false;
       return;
@@ -180,7 +184,7 @@ export default class extends React.Component {
 
     // 열 고개 대답을 받았을 때
     if (this.state.Qnum < 10 && (answerYes || answerNo)) {
-      this.playSound(soundAnswer);
+      this.replaySound(soundAnswer);
       if (answerYes) {
         this.think(true);
       } else if (answerNo) {
@@ -192,13 +196,13 @@ export default class extends React.Component {
         this.ask();
       } else if (figureOut) {
         this.stopSound(soundBGM);
-        this.playSound(soundFigureOut);
+        this.replaySound(soundFigureOut);
         this.setState({
           askIsThat: false,
           GameState: "End",
         });
       } else {
-        this.playSound(soundFailed);
+        this.replaySound(soundFailed);
         this.setState({
           askIsThat: false,
           GameState: "Failed",
@@ -282,14 +286,6 @@ function StartPage() {
 }
 
 function QuestionPage({ left, right, mid, askIsThat, Qnum }) {
-  function askText({ mid, askIsThat }) {
-    switch (askIsThat) {
-      case true:
-        return `${mid}(이)니?`;
-      case false:
-        return `${mid}보다 작니?`;
-    }
-  }
   function PressYes() {
     pressed = true;
     answerYes = true;
@@ -303,7 +299,15 @@ function QuestionPage({ left, right, mid, askIsThat, Qnum }) {
     answerBack = true;
   }
   function PressHelp() {
-    Alert.alert("도움말", "자기보다 작냐고 물어보면 작은게 아니다!");
+    Alert.alert("도움말", "자신이 자신보다 작냐고 물어보면\n작은게 아니다!");
+  }
+  function askText({ mid, askIsThat }) {
+    switch (askIsThat) {
+      case true:
+        return `${mid}(이)니?`;
+      case false:
+        return `${mid}보다 작니?`;
+    }
   }
   function getGradientColor({ Qnum }) {
     if (Qnum > 5) {
@@ -331,11 +335,11 @@ function QuestionPage({ left, right, mid, askIsThat, Qnum }) {
         </View>
 
         <View style={view.QuestionView}>
-          {/* <Text style={question.text}>
-            {left}, {mid}, {right}
-          </Text> */}
           <Text style={question.text}>생각한 숫자는</Text>
           <Text style={question.text}>{text}</Text>
+          {/* <Text style={question.text}>
+            {left} {mid} {right}
+          </Text> */}
         </View>
 
         <TouchableHighlight
@@ -428,7 +432,7 @@ function AnswerPage({ mid, Qnum }) {
     answerBack = true;
   }
   function PressHelp() {
-    Alert.alert("도움말", "자기보다 작냐고 물어보면 작은게 아니다!");
+    Alert.alert("도움말", "자신이 자신보다 작냐고 물어보면\n작은게 아니다!");
   }
   return (
     <LinearGradient
@@ -436,12 +440,12 @@ function AnswerPage({ mid, Qnum }) {
       colors={["#e1eec3", "#f05053"]}
       style={view.container}
     >
-      <StatusBar barStyle="black-content" />
+      <StatusBar barStyle="dark-content" />
       <View style={view.statusbarSide} />
       <View style={view.upSide}>
         <View style={view.menu}>
           <TouchableHighlight style={view.help} type={View} onPress={PressHelp}>
-            <Text style={answer.backText}>?</Text>
+            <Text style={answer.backText2}>?</Text>
           </TouchableHighlight>
 
           <Text style={question.answerQnum}>남은 고개: {10 - Qnum}</Text>
@@ -472,7 +476,7 @@ function AnswerPage({ mid, Qnum }) {
         </TouchableHighlight>
       </View>
 
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" /> */}
     </LinearGradient>
   );
 }
@@ -524,7 +528,7 @@ const view = StyleSheet.create({
     flex: 0.2,
   },
   upSide: {
-    flex: 3,
+    flex: 6,
     // alignItems: "center",
     // justifyContent: "center",
     // minHeight: "75%",
@@ -542,14 +546,13 @@ const view = StyleSheet.create({
     // paddingVertical: "7%",
   },
   help: {
-    color: "gray",
     fontWeight: "bold",
     borderRadius: 100,
     fontSize: 30,
     width: 30,
     alignItems: "center", // 글자 정렬
     alignSelf: "flex-end", // flexDirection에 수직인 방향으로 자신을 정렬
-    backgroundColor: "gray",
+    // backgroundColor: "gray",
     marginHorizontal: 17,
   },
   QuestionView: {
@@ -675,6 +678,11 @@ const answer = StyleSheet.create({
   },
   backText: {
     color: "white",
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  backText2: {
+    color: "black",
     fontWeight: "bold",
     fontSize: 30,
   },
